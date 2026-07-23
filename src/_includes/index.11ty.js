4aebe1a1ac8a07a3c910983fcfe85a561f2e10fa
Aram Zucker-Scharff <aramzs@hacktext.com>
2026-07-23 00:23:42 -0400

    Setting up better styling container hopefully

--- src/_includes/index.11ty.js
+++ src/_includes/index.11ty.js
@@ -25,24 +25,26 @@ module.exports = async function (data) {
 	let insert = {
 		template: "index",
 		content: /*html*/ `
-	<section class="hero scroll-hint-container">
-    	<div class="scroll-hint">
-      		<svg viewBox="0 0 24 24"><polyline points="6 9 12 15 18 9"/></svg>
-      		Scroll down
-    	</div>
-	</section>
-	<section class="hero">
-		<!--  <p class="sub-title">SUBTITLE</p>  
-		<a href="#" class="btn">SOME BUTTON</a> -->
-		<p class="left">where water is most scarce....</p>
-	</section>
-	<section class="hero">
-    	<p class="right">America grows its thirstiest crop</p>
-	</section>
-	<section class="hero title-section">
-		<h1 class="center">ALFALFA</h1>
-		<h3 class="center">An eco-noir by Jack Fessenden</h3>
-	</section>
+	<div class="home">
+		<section class="hero scroll-hint-container">
+			<div class="scroll-hint">
+				<svg viewBox="0 0 24 24"><polyline points="6 9 12 15 18 9"/></svg>
+				Scroll down
+			</div>
+		</section>
+		<section class="hero">
+			<!--  <p class="sub-title">SUBTITLE</p>  
+			<a href="#" class="btn">SOME BUTTON</a> -->
+			<p class="left">where water is most scarce....</p>
+		</section>
+		<section class="hero">
+			<p class="right">America grows its thirstiest crop</p>
+		</section>
+		<section class="hero title-section">
+			<h1 class="center">ALFALFA</h1>
+			<h3 class="center">An eco-noir by Jack Fessenden</h3>
+		</section>
+	</div>
   <section class="pages-container">
   	${renderedGroup}
   </section>
